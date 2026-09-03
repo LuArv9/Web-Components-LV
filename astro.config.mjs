d78b50216324e55cbaf036ca5173487ac4361b3e
Jovan Lopez <49563725+jovanlopez32@users.noreply.github.com>
2026-09-03 13:24:58 -0600

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [react()],
   site: 'https://jovanlopez32.github.io',
+  base: '/ds-web-components-v6v7',
 
   vite: {
     plugins: [tailwindcss()]
